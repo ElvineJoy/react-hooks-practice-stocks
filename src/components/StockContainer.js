e09af158ebe7a6524ddc2d77d5fbce4176677f15
ElvineJoy <elvinkuiyungest@gmail.com>
2024-06-07 13:32:43 +0300

    Rendering all stocks done

--- src/components/StockContainer.js
+++ src/components/StockContainer.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Stock from "./Stock";
 
 function StockContainer() {
+  const[stocks, setStocks] = useState([]);
+  
+  useEffect(() => {
+    fetch("http://localhost:3001/stocks")
+    .then(r => r.json())
+    .then(stocks => {{
+      setStocks(stocks)
+      console.log(stocks)
+    }})
+  }, []);
+
+  const stockItem = stocks.map(stock => (
+    <Stock 
+      key = {stock.id}
+      name ={stock.name}
+      ticker = {stock.ticker}
+      price = {stock.price}
+    />
+  ));
+
+
+  
+
   return (
     <div>
       <h2>Stocks</h2>
-      {/* render stock list here*/}
+      <ul>{stockItem}</ul>
     </div>
   );
 }
